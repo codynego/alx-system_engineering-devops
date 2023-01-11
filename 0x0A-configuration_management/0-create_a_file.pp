2212617d6637fd83310638da8161c6dc62058a21
Abednego Emonena <emonenaabednego@gmail.com>
2023-01-12 01:46:48 +0500

    fix a bug in create_a_file

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,8 +1,4 @@
 # File resource to create/update a file at /tmp/school
-# File permission is 0744
-# File owner is www-data
-# File group is www-data
-# File contains I love Puppet
 
 file {'/tmp/school':
   ensure  => file,
