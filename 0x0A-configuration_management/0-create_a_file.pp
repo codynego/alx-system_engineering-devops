23afde49aaf1058001acef320fffe0ab75cd63b1
Abednego Emonena <emonenaabednego@gmail.com>
2023-01-12 05:15:22 +0500

    fix a spelling error

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,9 +1,14 @@
 # File resource to create/update a file at /tmp/school
+# File permission is 0744
+# File owner is www-data
+# File group is www-data
+# File contains I love Puppet
 
 file {'/tmp/school':
   ensure  => file,
   mode    => '0744',
   owner   => 'www-data',
   group   => 'www-data',
-  centent => 'I love Puppet'
+  content => 'I love Puppet'
+
 }
