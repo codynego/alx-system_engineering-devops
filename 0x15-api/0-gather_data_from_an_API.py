dd51c52ef01a3e8e814ed77723705aac33300f49
Abednego Emonena <emonenaabednego@gmail.com>
2023-02-09 02:37:53 +0500

    a script that return a users task using its id

---
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -0,0 +1,40 @@
+#!/usr/bin/python3
+
+# a Python script that, using this REST API, for a given employee ID,
+# returns information about his/her TODO list progress
+
+import requests
+import json
+import sys
+
+
+base_url = 'https://jsonplaceholder.typicode.com'
+
+if __name__ == "__main__":
+    
+    user_id = sys.argv[1]
+
+    # get user info e.g https://jsonplaceholder.typicode.com/users/1/
+    user_url = '{}/users?id={}'.format(base_url, user_id)
+    
+    response = requests.get(user_url)
+    dict_response = json.loads(response.text)
+    
+    name = dict_response[0].get('name')
+    
+    tasks_url = '{}/todos?userId={}'.format(base_url, user_id)
+    task_response = requests.get(tasks_url)
+    task_data = json.loads(task_response.text)
+    total_task = len(task_data)
+    completed = 0
+    completed_task = []
+    
+    for task in task_data:
+        if task.get('completed') == True:
+            completed += 1
+            completed_task.append(task)
+
+    print("Employee {} is done with task({}/{}):"
+            .format(name, completed, total_task))
+    for task in completed_task:
+        print("\t {}".format(task.get('title')))
