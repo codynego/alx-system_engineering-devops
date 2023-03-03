1f5c0d28bf71dfda4ab930c350310794dac08350
codynego <emonenaabednego@gmail.com>
2023-03-04 00:39:54 +0500

    added 1-top_ten

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,18 +1,21 @@
 #!/usr/bin/python3
+"""
+Contains a function that works with Reddit API
+"""
 
 import requests
 
-""" Function that uses Reddit Api """
-
 
 def number_of_subscribers(subreddit):
-    """ Returns the number of subscriber for a given subreddit """
-
-    url = f"https://www.reddit.com/r/{subreddit}/about.json"
-    headers = {'User-Agent': 'Mozilla/5.0'}
-    response = requests.get(url, headers=headers)
-    if response.status_code == 200:
-        data = response.json()
-        return data['data']['subscribers']
-    else:
+    """returns the number of subscribers for a given subreddit"""
+    if subreddit is None or type(subreddit) is not str:
+        return 0
+    url = 'http://www.reddit.com/r/{}/about.json'.format(subreddit)
+    headers = {'User-Agent': '0x16-api_advanced:project:\
+            v1.0.0 (by /u/Ajiboye Adeleye Pius)'}
+    res = requests.get(url, headers=headers).json()
+    try:
+        subs = res.get('data').get('subscribers')
+        return subs
+    except Exception:
         return 0
